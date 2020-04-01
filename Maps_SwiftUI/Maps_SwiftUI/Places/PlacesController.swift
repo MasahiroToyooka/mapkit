c8418d568c6ffe9803c65dd3ecc93af9610a1445
MasahiroToyooka <ehara2481@gmail.com>
2020-04-01 22:59:25 +0900

    create action for selected annotation

--- Maps_SwiftUI/Maps_SwiftUI/Places/PlacesController.swift
+++ Maps_SwiftUI/Maps_SwiftUI/Places/PlacesController.swift
@@ -11,7 +11,7 @@ import LBTATools
 import MapKit
 import GooglePlaces
 
-class PlacesController: UIViewController, CLLocationManagerDelegate {
+class PlacesController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     let mapView = MKMapView()
     let locationManager = CLLocationManager()
@@ -21,6 +21,7 @@ class PlacesController: UIViewController, CLLocationManagerDelegate {
         
         view.addSubview(mapView)
         mapView.fillSuperview()
+        mapView.delegate = self
         mapView.showsUserLocation = true
         locationManager.delegate = self
         
@@ -40,7 +41,7 @@ class PlacesController: UIViewController, CLLocationManagerDelegate {
                 
                 let place = likelihood.place
                 
-                let annotation = MKPointAnnotation()
+                let annotation = PlaceAnnotation(place: place)
                 annotation.title = place.name
                 annotation.coordinate = place.coordinate
                 
@@ -50,6 +51,121 @@ class PlacesController: UIViewController, CLLocationManagerDelegate {
             self?.mapView.showAnnotations(self?.mapView.annotations ?? [], animated: false)
         }
     }
+    
+    class PlaceAnnotation: MKPointAnnotation {
+        let place: GMSPlace
+        
+        init(place: GMSPlace) {
+            self.place = place
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        if !(annotation is PlaceAnnotation) { return nil}
+        
+        let annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "id")
+        
+//        annotationView.canShowCallout = true
+        
+        if let placeAnnotation = annotation as? PlaceAnnotation {
+            let types = placeAnnotation.place.types
+            
+            if let firstType = types?.first {
+                if firstType == "bar" {
+                    annotationView.image = #imageLiteral(resourceName: "tourist")
+                } else if firstType == "restaurant" {
+                    annotationView.image = #imageLiteral(resourceName: "restaurant")
+                } else {
+                    annotationView.image = #imageLiteral(resourceName: "bar")
+                }
+            }
+        }
+        return annotationView
+    }
+    
+    var currentCustomCallout: UIView?
+    
+        func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+            currentCustomCallout?.removeFromSuperview()
+            
+            let customCalloutContainer = UIView(backgroundColor: .white)
+            
+    //        customCalloutContainer.frame = .init(x: 0, y: 0, width: 100, height: 200)
+            
+            view.addSubview(customCalloutContainer)
+            
+            customCalloutContainer.translatesAutoresizingMaskIntoConstraints = false
+            
+            let widthAnchor = customCalloutContainer.widthAnchor.constraint(equalToConstant: 100)
+            widthAnchor.isActive = true
+            let heightAnchor = customCalloutContainer.heightAnchor.constraint(equalToConstant: 200)
+            heightAnchor.isActive = true
+            customCalloutContainer.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            customCalloutContainer.bottomAnchor.constraint(equalTo: view.topAnchor).isActive = true
+            
+            customCalloutContainer.layer.borderWidth = 2
+            customCalloutContainer.layer.borderColor = UIColor.darkGray.cgColor
+            
+            customCalloutContainer.setupShadow(opacity: 0.2, radius: 5, offset: .zero, color: .darkGray)
+            customCalloutContainer.layer.cornerRadius = 5
+            
+            currentCustomCallout = customCalloutContainer
+            
+            // load the spinner
+            let spinner = UIActivityIndicatorView(style: .large)
+            spinner.color = .darkGray
+            spinner.startAnimating()
+            customCalloutContainer.addSubview(spinner)
+            spinner.fillSuperview()
+            
+            // look up our photo
+            
+            guard let placeId = (view.annotation as? PlaceAnnotation)?.place.placeID else { return }
+            
+            client.lookUpPhotos(forPlaceID: placeId) { [weak self] (metadatalist, err) in
+                if let err = err {
+                    print("Error when looking up photos:", err)
+                    return
+                }
+                
+                guard let firstPhotoMetadata = metadatalist?.results.first else { return }
+                
+                self?.client.loadPlacePhoto(firstPhotoMetadata) { (image, err) in
+                    if let err = err {
+                        print("Failed to load photo for place:", err)
+                        return
+                    }
+                    
+                    guard let image = image else { return }
+                    
+                    if image.size.width > image.size.height {
+                        // w1/h1 = w2/h2
+                        let newWidth: CGFloat = 300
+                        let newHeight = newWidth / image.size.width * image.size.height
+                        widthAnchor.constant = newWidth
+                        heightAnchor.constant = newHeight
+                    } else {
+                        let newHeight: CGFloat = 200
+                        let newWidth = newHeight / image.size.height * image.size.width
+                        widthAnchor.constant = newWidth
+                        heightAnchor.constant = newHeight
+                    }
+                    
+                    // how do i put it into the red view
+                    let imageView = UIImageView(image: image, contentMode: .scaleAspectFill)
+                    customCalloutContainer.addSubview(imageView)
+                    imageView.layer.cornerRadius = 5
+                    imageView.fillSuperview()
+                    
+                    // label
+                    let labelContainer = UIView(backgroundColor: .white)
+                    let nameLabel = UILabel(text: (view.annotation as? PlaceAnnotation)?.place.name, textAlignment: .center)
+                    labelContainer.stack(nameLabel)
+                    customCalloutContainer.stack(UIView(), labelContainer.withHeight(30))
+                }
+            }
+        }
     
     fileprivate func requestForLocationAuthorization() {
         locationManager.requestWhenInUseAuthorization()
